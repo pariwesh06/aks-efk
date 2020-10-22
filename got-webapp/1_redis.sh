ec16479812eead9be9fc30d6bf46c309dad777d2
pariwesh06 <pariweshg@gmail.com>
2020-10-22 11:49:57 +0000

    updated

--- got-webapp/1_redis.sh
+++ got-webapp/1_redis.sh
@@ -3,5 +3,6 @@ kubectl create ns got-webapp
 # Creating deployment
 kubectl run redis --image redis --namespace got-webapp
 # Exposing deployment as service
-kubectl expose deployment redis --namespace got-webapp --port 6379
+# kubectl expose deployment redis --namespace got-webapp --port 6379
+kubectl expose pod redis --namespace got-webapp --port 6379 --type LoadBalancer
 
